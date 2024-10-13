8f0dc67b96177eb6b17ed15cd9540401f0d9b1ac
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-10-13 12:37:58 +0300

    tg

--- src/pages/tg.tsx
+++ src/pages/tg.tsx
@@ -2,11 +2,34 @@ import { HeadFC } from 'gatsby'
 import * as React from 'react'
 import { useEffect, useRef } from 'react'
 
+interface TelegramUser {
+  id: number // 315833496
+  username: string // "alexandrm85"
+  first_name: string // "Alexandr"
+  last_name: string // "Marchenko"
+  auth_date: number // 1728808910
+  hash: string // "2a49738a3f0b7a14149e1846b3c3c25c56e999ee21aff3fc93060452f7fd9bf7"
+}
+
+declare global {
+  interface Window {
+    onTelegramAuth: (user: TelegramUser) => void
+    telegram?: TelegramUser
+  }
+}
+
 const Page: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    if (!ref.current || ref.current.children.length) {
+    if (!ref.current || ref.current.children.length || typeof window === 'undefined') {
       return
+    }
+    window.onTelegramAuth = (user: TelegramUser) => {
+      console.log('onTelegramAuth', user)
+      window.telegram = user
+      fetch('https://europe-west3-iplantalks.cloudfunctions.net/tga', { method: 'POST', headers: { 'content-type': 'application/json; charset=utf-8' }, body: JSON.stringify(user) })
+        .then((r) => r.json())
+        .then(console.log)
     }
     const script = document.createElement('script')
     script.src = 'https://telegram.org/js/telegram-widget.js?22'
