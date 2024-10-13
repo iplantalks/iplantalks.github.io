fb70ca1c881a549f4b59c7336647f4422bca0e3c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-10-13 11:33:46 +0300

    tg

--- src/pages/tg.tsx
+++ src/pages/tg.tsx
@@ -3,53 +3,32 @@ import * as React from 'react'
 import { useEffect, useRef } from 'react'
 
 const Page: React.FC = () => {
-  // const divRef = useRef<HTMLDivElement>(null)
-  // const scriptRef = useRef<HTMLScriptElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    // scriptRef.current?.remove()
-    // window['onTelegramAuth'] = (user) => {
-    //   console.log('onTelegramAuth', user)
-    // }
-    // scriptRef.current = document.createElement('script')
-    // divRef.current?.after(scriptRef.current)
-    // const siblings = divRef.current?.parentElement?.children
-    // return () => {
-    //   // destroy the script element on unmount
-    //   scriptRef.current?.remove()
-    //   // We also need to remove the rendered iframe
-    //   if (siblings) {
-    //     for (let i = 0; i < siblings.length; i++) {
-    //       const element = siblings.item(i)
-    //       if (element instanceof HTMLIFrameElement && element.src.includes('oauth.telegram.org')) {
-    //         element.remove()
-    //         break
-    //       }
-    //     }
-    //   }
-    // }
+    if (!ref.current || ref.current.children.length) {
+      return
+    }
+    const script = document.createElement('script')
+    script.src = 'https://telegram.org/js/telegram-widget.js?22'
+    script.async = true
+    script.setAttribute('data-telegram-login', 'manymacbot')
+    script.setAttribute('data-size', 'medium')
+    script.setAttribute('data-radius', '5')
+    script.setAttribute('data-onauth', 'console.log(user)')
+    script.setAttribute('data-request-access', 'write')
+
+    ref.current.appendChild(script)
   }, [])
+
   return (
     <main>
       <div className="container py-5">
         <h2>Telegram Demo</h2>
-
-        <script
-          async
-          src="https://telegram.org/js/telegram-widget.js?22"
-          data-telegram-login="manymacbot"
-          data-size="medium"
-          data-radius="5"
-          data-onauth="console.log(user)"
-          data-request-access="write"
-        ></script>
+        <div ref={ref} />
       </div>
     </main>
   )
 }
 
 export default Page
-export const Head: HeadFC = () => (
-  <>
-    <title>Telegram Demo</title>
-  </>
-)
+export const Head: HeadFC = () => <title>Telegram Demo</title>
